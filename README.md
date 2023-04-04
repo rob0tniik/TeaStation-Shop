229e323bf800353fd0683412722b93d3ac3864d8
roob-cell <dbatos01@gmail.com>
2023-04-04 19:33:36 +0200

    Dry JS

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Javascript clean up, make it dry, with querySelectorAll insted of 27 id's
-
 # Truly form functionality with Formspree
--- app.js
+++ app.js
@@ -8,12 +8,7 @@ const date = (document.getElementById(
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
-const index = document.getElementById("borrar-index");
-const skills = document.getElementById("borrar-skills")
-const about = document.getElementById("borrar-about")
-const products = document.getElementById("borrar-products")
-const services = document.getElementById("borrar-services")
-const contact = document.getElementById("borrar-contact")
+const links = document.querySelectorAll(".nav-link")
 
 // show nav
 navBtn.addEventListener("click", () => {
@@ -23,22 +18,8 @@ navBtn.addEventListener("click", () => {
 navClose.addEventListener("click", () => {
   navbar.classList.remove("showNav");
 });
-index.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
-});
-skills.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
-});
-
-about.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
-});
-products.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
-});
-services.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
-});
-contact.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
-});
+links.forEach(link => {
+  link.addEventListener("click", () => {    
+    navbar.classList.remove("showNav");
+  })
+})
